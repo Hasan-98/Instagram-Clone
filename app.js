adf68ccf3d42b50da9ee01f328e7492a77aed308
Hasan-98 <hasanmehmood458@gmail.com>
2022-11-01 22:22:58 +0500

    middleware addded

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 
+
+const customerMiddleware = (req , res  , next)=>{
+    console.log('Middle ware executed')
+    next()
+}
+
+
+
+//app.use(customerMiddleware)
 app.get('/' , (req , res)=>{
     res.send('hello world this is testin23g 123')
+})
+app.get('/about' ,customerMiddleware, (req , res)=>{
+    console.log('about')
+    res.send('Abount page')
 })
 
 
