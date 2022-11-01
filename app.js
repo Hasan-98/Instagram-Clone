d4d08a560d551593a1d35a704399d20a3efa4e35
Hasan-98 <hasanmehmood458@gmail.com>
2022-11-01 23:08:42 +0500

    Schema added

--- app.js
+++ app.js
@@ -4,31 +4,17 @@ const app = express()
 const PORT = 5000
 const mongoose = require('mongoose')
 const {MONGOURI} = require('./key')
-const customerMiddleware = (req , res  , next)=>{
-    console.log('Middle ware executed')
-    next()
-}
+require('./models/user')
 
-mongoose.connect(
-    MONGOURI)
+
+
+mongoose.connect(MONGOURI)
 mongoose.connection.on('connected', ()=>{
     console.log('Connected to MongoDb')
 })
 mongoose.connection.on('Error', (err)=>{
     console.log('Error connected to MongoDb', err)
 })
-
-
-
-//app.use(customerMiddleware)
-app.get('/' , (req , res)=>{
-    res.send('hello world this is testin23g 123')
-})
-app.get('/about' ,customerMiddleware, (req , res)=>{
-    console.log('about')
-    res.send('Abount page')
-})
-
 
 app.listen(PORT , ()=>{
     console.log('Server is running on' ,  PORT)
